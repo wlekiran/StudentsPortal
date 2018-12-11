bd56fa27b9602075e59e029ff36c371277d56075
kiran <kiran.hatesiit@gmail.com>
2018-12-11 14:14:37 +0530

    added students homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ForgorPasswordComponent } from './forgor-password.component';
+import { LoginComponent } from './login.component';
+import { StudenthomeComponent } from './Students/studenthome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForgorPasswordComponent,
+    LoginComponent,
+    StudenthomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+      {path: 'forgotpassword', component: ForgorPasswordComponent},
+      {path: 'studenthome', component: StudenthomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
